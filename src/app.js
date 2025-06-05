60837e90c5f6e5b8051b8b92e17742762a0b8479
pradeeprawat-jft <pradeep.rawat@gellyfishtechnologies.com>
2025-06-05 11:29:18 +0530

    routing and req handler

--- src/app.js
+++ src/app.js
@@ -11,8 +11,40 @@ app.use("/", (req, res, next) => {
 
 
 app.get("/hello", (req, res) => {
-    console.log('Request received at /hello path');
     res.send('Hello, Hello!')
+});
+
+// This route will match both "/hello" and "/helo"
+// The regex pattern allows for an optional 'l' before the 'o'
+// app.get("/hell?o", (req, res) => {
+//     res.send('ello, Hello!')
+// });
+
+
+// This route will match both "/hello" and "/helloo"
+// The regex pattern allows for one or more 'l's  before the 'o'
+// app.get("/hell+o", (req, res) => {
+//     res.send('Hello, Hello!')
+// });
+
+
+
+app.patch("/hello/:id", (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    console.log(`Request body: ${JSON.stringify(body)}`);
+    console.log(`Request received to update Hello with ID: ${id}`);
+    res.send('Hello, Hello! updated!', { id });
+});
+
+app.post("/hello", (req, res) => {
+    const body = req.body;
+    res.send('Hello Added! & POST method used ', body);
+});
+
+app.delete("/hello/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Hello with ID ${id} deleted!`);
 });
 
 
